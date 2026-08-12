e9b402b2aa2bb797d6ab9effecfec8fba84abf56
rczajkadev <rafalcz29@gmail.com>
2026-08-12 19:39:55 +0200

    Add Firefox-specific manifest settings

--- wxt.config.mjs
+++ wxt.config.mjs
@@ -1,15 +1,23 @@
 import { defineConfig } from 'wxt';
 
+const geckoSettings = {
+  id: 'yt-smart-tv@rafalczajka.github.io',
+  data_collection_permissions: {
+    required: ['none']
+  }
+};
+
 export default defineConfig({
   srcDir: 'src',
   outDir: 'dist',
   modules: ['@wxt-dev/module-svelte'],
-  manifest: {
+  manifest: ({ browser }) => ({
     name: 'YouTube Smart TV',
     description: 'Open YouTube in TV mode with one click.',
     version: '1.0.6',
     homepage_url: 'https://github.com/rafalczajka/yt-smart-tv',
     permissions: ['declarativeNetRequest', 'storage'],
-    host_permissions: ['https://*.youtube.com/*']
-  }
+    host_permissions: ['https://*.youtube.com/*'],
+    browser_specific_settings: browser === 'firefox' ? { gecko: geckoSettings } : undefined
+  })
 });
